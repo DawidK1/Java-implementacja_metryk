f79d8aad103545852b949bbeec9bdb96ae6c8b90
Andrzej1312 <ajtoja@gmail.com>
2016-05-16 19:31:43 +0200

    Create MetricCanberr.java

---
+++ test/src/metrics/MetricCanberr.java
@@ -0,0 +1,23 @@
+
+package metrics;
+
+import java.util.List;
+
+public class MetricCanberr extends Metric {
+
+	public MetricCanberr(List<Double> v1, List<Double> v2) {
+		super(v1, v2);
+
+	}
+
+	@Override
+	double distance() {
+	double result=0;
+		int i;
+		for(i = 0 ; i < dim ; i++){
+			result=result+Math.abs(vec1.get(i)-vec2.get(i))/Math.abs(vec1.get(i)+vec2.get(i));
+		}
+	return result;
+	}
+
+}
